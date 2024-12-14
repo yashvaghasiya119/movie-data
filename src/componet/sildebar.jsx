24337991280d83469370df1b7265e654dd8a1ca6
yashvaghasiya119 <yashvaghasiya360@gmail.com>
2024-12-14 18:59:38 +0530

    for tech taliwind

--- src/componet/sildebar.jsx
+++ src/componet/sildebar.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router";
+
 export function Sildebar() {
 
     return <>
@@ -7,7 +9,14 @@ export function Sildebar() {
                     <i className="ri-tv-fill" style={{ color: "#6556cd" }}></i>
                     <span className="left-span-1"> Yash movies</span>
                 </h2>
-                <span>New feed</span>
+                <span className="left-span-2">New feed</span>
+                <div className="all-links">
+                    <Link className="link">Tranding</Link>
+                    <Link className="link">Popular</Link>
+                    <Link className="link">Movies</Link>
+                    <Link className="link">Tv show</Link>
+                    <Link className="link">People</Link>
+                </div>
             </div>
         </div>
     </>
