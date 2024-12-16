221d4807dd47806173f082514ff55807305e9f69
yashvaghasiya119 <yashvaghasiya360@gmail.com>
2024-12-16 12:31:22 +0530

    16 12:31

--- src/AllRouts/home.jsx
+++ src/AllRouts/home.jsx
@@ -1,13 +1,31 @@
+import { useEffect, useState } from "react"
 import { Sildebar } from "../componet/sildebar"
 import { Navigation } from "../componet/top-navigation"
+import { da } from "../componet/axiosdata"
 
 export function Home(){
     document.title = "Home page"
+
+    let [wallpaper,setwallpaper]=useState(null)
+
+    async function wallpaperfunc(){
+         try {
+              let d = await da.get(`/trending/all/day`);
+              let random =Math.floor(Math.random()*20)
+              setwallpaper(d.data.results[random].original_title);
+            //   console.log();
+            } catch (error) {
+              console.log(error);
+            }
+          };
+    useEffect(()=>{
+      !wallpaper &&  wallpaperfunc()
+    },[])
     return<>
    <div className="home">
-    <Sildebar/>
+    <Sildebar />
     <div className="right">
-        <Navigation/>
+        <Navigation />
     </div>
    </div>
     </>
--- src/componet/top-navigation.jsx
+++ src/componet/top-navigation.jsx
@@ -1,58 +1,122 @@
-import { data, Link } from 'react-router'
-import './compo.css'
-import { useEffect, useState } from 'react'
-import { da } from './axiosdata'
+// import { data, Link } from 'react-router'
+// import './compo.css'
+// import { useEffect, useState } from 'react'
+// import { da } from './axiosdata'
+
+// export function Navigation() {
+//   // React state for the query
+//   const [query, setquery] = useState("salar");
+//   let [data1,setdata]=useState(null)
+//   let[loading,setloading]=useState(true)
+
+//   let icon = query.length > 0;
+
+//   const myfun = async () => {
+//     try {
+//       // const d = await da.get(`movie/now_playing?query=${query}`);
+//       let d = await da.get(`search/multi?query=${query}`)
+//       setdata(d.data.results)
+//       console.log(d.data.results[0].original_title);
+
+//       setloading(false)
+
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+
+// useEffect(()=>{
+//     myfun()
+// },[query])
+// console.log(data1);
+
+
+//   // 
+//     if(loading)return <h1>Loading</h1>
+//     return <>
+//         <div className="navi-main">
+
+//             <i className="ri-search-eye-line nav-icon"></i>
+//             <input type="text" placeholder="search anything" value={query} onChange={(e)=>setquery(e.target.value)}/>
+//             {icon? <i className="ri-close-fill nav-icon" onClick={()=>setquery("")}></i>:"" }
+//         </div>
+//         <div className="searchitem">
+//           {
+//            data1 ||  data1.map((cur,index)=>{
+//                 return <Link key={index} className="navlink">
+//                   <img src="" alt="" />
+//                   <span>{cur.name || cur.title || cur.original_name || cur.original_title}</span>
+//                   <hr style={{ border: "2px solid grey ", width: "100%", marginTop: 0 }} /> 
+//               </Link>
+//             })
+//           }
+
+
+//         </div>
+
+//     </>
+// }
+
+//gpt
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router';
+import './compo.css';
+import { da } from './axiosdata';
+import noimg from  './no-img.jpeg'
 
 export function Navigation() {
-  // React state for the query
   const [query, setquery] = useState("");
-  let [data,setdata]=useState(null)
-  let[loading,setloading]=useState(true)
+  const [data1, setdata] = useState([]);
+  const [loading, setloading] = useState(false);
 
-  // This will check if the query length is more than 0
-  let icon = query.length > 0;
+  const icon = query.length > 0;
 
-//   // Define async function inside the component
   const myfun = async () => {
+    if (!query) return; // Prevent API call if query is empty
     try {
-      const d = await da.get(`movie/now_playing?query=${query}`);
-      setdata(d.data.results)
-      // console.log(d.data.results);
-      setloading(false)
-      
+      let d = await da.get(`search/multi?query=${query}`);
+      setdata(d.data.results);
+      setloading(false);
     } catch (error) {
       console.log(error);
+      setloading(false); // Stop loading if there's an error
     }
   };
 
-useEffect(()=>{
-    myfun()
-},[query])
+  useEffect(() => {
+    myfun();
+  }, [query]);
 
+  if (loading) return <h1>Loading</h1>;
 
-  // 
-    if(loading)return <h1>Loading</h1>
-    return <>
-        <div className="navi-main">
-
-            <i className="ri-search-eye-line nav-icon"></i>
-            <input type="text" placeholder="search anything" value={query} onChange={(e)=>setquery(e.target.value)}/>
-            {icon? <i className="ri-close-fill nav-icon" onClick={()=>setquery("")}></i>:"" }
-        </div>
-        <div className="searchitem">
-            {/* <Link className="navlink">
-                <img src="" alt="" />
-                <p>this is seach</p>
+  return (
+    <>
+      <div className="navi-main">
+        <i className="ri-search-eye-line nav-icon"></i>
+        <input
+          type="text"
+          placeholder="search anything"
+          value={query}
+          onChange={(e) => setquery(e.target.value)}
+        />
+        {icon && (
+          <i
+            className="ri-close-fill nav-icon"
+            onClick={() => setquery("")}
+          ></i>
+        )}
+      </div>
+      <div className="searchitem">
+      
+          {data1.map((cur, index) => (
+            <Link key={index} className="navlink">
+              <img src={ cur.backdrop_path || cur.profile_path ? `https://image.tmdb.org/t/p/original/${cur.backdrop_path || cur.profile_path} ` : noimg}  />
+              <span>{cur.name || cur.title || cur.original_name || cur.original_title}</span>
+              {/* <hr style={{ border: "2px solid grey ", width: "100%", marginTop: 0 }} /> */}
             </Link>
-            <hr style={{ border: "2px solid grey ", width: "100%", marginTop: 0 }} /> */}
-
-        </div>
-     {
-      data ? data.map((vr,index)=>{
-        return <div key={index} style={{color:"white"}}>{vr.original_title}
-          </div>
-         
-       }) : "lo"
-     }
+          ))
+       }
+      </div>
     </>
+  );
 }
